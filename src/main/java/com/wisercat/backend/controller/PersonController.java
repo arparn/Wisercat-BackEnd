df5848bd3cb91c04d098d56060e1c17e03cd971b
Artur-Aleksander <arturaleksander.parnoja@1ot.mobi>
2023-06-16 22:47:55 +0300

    add initial test functionality

--- src/main/java/com/wisercat/backend/controller/PersonController.java
+++ src/main/java/com/wisercat/backend/controller/PersonController.java
@@ -2,6 +2,9 @@ package com.wisercat.backend.controller;
 
 import com.wisercat.backend.service.PersonService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
 
 	private final PersonService personService;
+
+	@GetMapping
+	public ResponseEntity<String> getPerson() {
+		return new ResponseEntity<>(personService.getPerson(), HttpStatus.OK);
+	}
 
 }
--- src/main/java/com/wisercat/backend/service/PersonService.java
+++ src/main/java/com/wisercat/backend/service/PersonService.java
@@ -3,10 +3,16 @@ package com.wisercat.backend.service;
 import com.wisercat.backend.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PersonService {
 
 	private final PersonRepository personRepository;
+
+	@Transactional
+	public String getPerson() {
+		return personRepository.findAll().get(0).getName();
+	}
 }
